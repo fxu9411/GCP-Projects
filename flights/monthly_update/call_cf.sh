508e470b25824853ed586316a64a185215e1069e
Frank Xu <franklolxu@gmail.com>
2020-01-22 23:26:11 -0500

    Upload call_cf.sh

--- flights/monthly_update/call_cf.sh
+++ flights/monthly_update/call_cf.sh
@@ -14,7 +14,7 @@ TOKEN=$4
 
 URL="https://${REGION}-${PROJECT}.cloudfunctions.net/${PATH}"
 
-echo {\"year\":\"2015\"\,\"month\":\"03\"\,\"bucket\":\"${BUCKET}\", \"token\":\"${TOKEN}\"} > /tmp/message
+echo {\"year\":\"2018\"\,\"month\":\"01\"\,\"bucket\":\"${BUCKET}\", \"token\":\"${TOKEN}\"} > /tmp/message
 cat /tmp/message
 
 curl -X POST "https://${REGION}-${PROJECT}.cloudfunctions.net/$URL" -H "Content-Type:application/json" --data-binary @/tmp/message

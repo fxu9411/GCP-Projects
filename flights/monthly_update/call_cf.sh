66bcd190280a6e3e06cbb6959c4a95291c5b4f4f
Frank Xu <franklolxu@gmail.com>
2020-01-22 23:47:49 -0500

    update call_cf.sh

--- flights/monthly_update/call_cf.sh
+++ flights/monthly_update/call_cf.sh
@@ -2,7 +2,7 @@
 
 if [ "$#" -ne 4 ]; then
     echo "Usage: ./call_cf.sh  destination-bucket-name compute-region ingest-url  personal-access-token"
-    echo "   eg: ./call_cf.sh  cloud-training-demos-ml us-central1 ingest_flights_udwaxx86mVygAmOazUcijW8zBXWNxEVM  DI8TWPzTedNF0b3B8meFPxXSWw6m3bKG"
+    echo "   eg: ./call_cf.sh  data_science_on_gcp_bucket us-east1 ingest_flights DI8TWPzTedNF0b3B8meFPxXSWw6m3bKG"
     exit
 fi
 
@@ -14,7 +14,7 @@ TOKEN=$4
 
 URL="https://${REGION}-${PROJECT}.cloudfunctions.net/${PATH}"
 
-echo {\"year\":\"2018\"\,\"month\":\"01\"\,\"bucket\":\"${BUCKET}\", \"token\":\"${TOKEN}\"} > /tmp/message
+echo {\"year\":\"2018\"\,\"month\":\"02\"\,\"bucket\":\"${BUCKET}\", \"token\":\"${TOKEN}\"} > /tmp/message
 cat /tmp/message
 
 curl -X POST "https://${REGION}-${PROJECT}.cloudfunctions.net/$URL" -H "Content-Type:application/json" --data-binary @/tmp/message

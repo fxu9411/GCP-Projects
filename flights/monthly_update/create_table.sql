6ba38218c5b06e1a236b28e599f844efeb399924
Frank Xu <franklolxu@gmail.com>
2020-01-27 14:58:19 -0500

    fix sql file

--- flights/monthly_update/create_table.sql
+++ flights/monthly_update/create_table.sql
@@ -1,7 +1,8 @@
 create database if not exists bts;
   use bts;
   drop table if exists flights;
-  create table flights ( FL_DATE date, UNIQUE_CARRIER varchar(16), AIRLINE_ID varchar(16), CARRIER varchar(16),
+  create table flights (
+    FL_DATE date, UNIQUE_CARRIER varchar(16), AIRLINE_ID varchar(16), CARRIER varchar(16),
     FL_NUM integer,
     ORIGIN_AIRPORT_ID varchar(16),
     ORIGIN_SEQ_ID varchar(16),
@@ -25,3 +26,4 @@ create database if not exists bts;
     DIVERTED float,
     DISTANCE float,
     INDEX (FL_DATE), INDEX (ORIGIN_AIRPORT_ID), INDEX(ARR_DELAY), INDEX(DEP_TIME), INDEX(DEP_DELAY)
+  );

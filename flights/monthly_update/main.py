ac92f358b70dd622aa034df9ad58c0be4dce88e5
Frank Xu <franklolxu@gmail.com>
2020-02-05 01:08:27 -0500

    update call_cf.sh

--- flights/monthly_update/main.py
+++ flights/monthly_update/main.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-
-import os
 import logging
 from flask import escape
 from ingest_flights import *
@@ -11,19 +8,18 @@ def ingest_flights(request):
         logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
         json = request.get_json()
 
-        if escape(json['token']) != 'QHh3ST4mv6nIfFZaLyXZbeXtGQqkU9Im':
+        if escape(json['token']) != 'DI8TWPzTedNF0b3B8meFPxXSWw6m3bKG':
             logging.info('Ignoring request without valid token')
             return
 
         year = escape(json['year']) if 'year' in json else None
         month = escape(json['month']) if 'month' in json else None
-        bucket = escape(json['bucket'])
+        bucket = escape(json['bucket'])  # required
 
         if year is None or month is None or len(year) == 0 or len(month) == 0:
             year, month = next_month(bucket)
-        logging.debug('Ingesting year = {}, month = {}'.format(year, month))
-        gcs_file = ingest(year, month, bucket)
-        logging.info('Success ... ingested to {}'.format(gcs_file))
-
+        logging.debug('Ingesting year={} month={}'.format(year, month))
+        gcsfile = ingest(year, month, bucket)
+        logging.info('Success ... ingested to {}'.format(gcsfile))
     except DataUnavailable as e:
         logging.info('Try again later: {}'.format(e.message))
